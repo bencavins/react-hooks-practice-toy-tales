edd984ba123f840711fe9d8511f51d84869d86ca
Ben Cavins <ben.cavins@flatironschool.com>
2023-02-07 11:26:07 -0500

    GET POST PATCH DELETE

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,56 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard(props) {
+  function handleDeleteClick(event) {
+    fetch(`http://localhost:3001/toys/${props.id}`, {
+      method: 'DELETE'
+    })
+      .then(resp => resp.json())
+      .catch(error => console.log(error))
+    
+    props.setToys(prev => {
+      return prev.filter(toy => {
+        return toy.id !== props.id
+      })
+    })
+  }
+
+  function handleLikeClick(event) {
+    fetch(`http://localhost:3001/toys/${props.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({likes: props.likes + 1})
+    })
+      .then(resp => resp.json())
+      .then(updated => {
+        props.setToys(prev => prev.map(toy => {
+          if (toy.id === props.id) {
+            return updated
+          } else {
+            return toy
+          }
+        }))
+      })
+      .catch(error => console.log(error))
+  }
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{props.name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={props.image}
+        alt={props.name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{props.likes} Likes </p>
+      <button 
+        className="like-btn"
+        onClick={handleLikeClick}>Like {"<3"}</button>
+      <button 
+        className="del-btn"
+        onClick={handleDeleteClick}>Donate to GoodWill</button>
     </div>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,9 +1,31 @@
 import React from "react";
 
-function ToyForm() {
+function ToyForm(props) {
+  function handleSubmit(event) {
+    event.preventDefault()
+    const newToy = {
+      name: event.target.name.value,
+      image: event.target.image.value
+    }
+    
+    fetch('http://localhost:3001/toys', {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    })
+      .then(resp => resp.json())
+      .catch(error => console.log(error))
+    
+    props.setToys(prev => [...prev, newToy])
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form 
+        className="add-toy-form" 
+        onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
